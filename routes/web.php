6a838c33df9aeb346b8fb391ce0bc01c119f4fc5
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-04-10 20:15:50 +0200

    Initial version

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Events\ChatMessage;
+use Illuminate\Support\Facades\Event;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/message',function() {
+   //Persistència
+    //
+    //
+
+   Event::dispatch(new ChatMessage('Missatge de prova'));
+//   event(new ChatMessage())
+});
+
+Route::get('chat/1',function() {
+   return view('chat');
+});

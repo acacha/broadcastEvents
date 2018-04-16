271cb538acf840916c6dbdb55caeb20615ecd1b0
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-04-16 20:21:38 +0200

    Sync

--- app/Http/Controllers/ChatMessagesController.php
+++ app/Http/Controllers/ChatMessagesController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Events\ChatMessage;
+use Auth;
 use Illuminate\Http\Request;
+use Notification;
 
 class ChatMessagesController extends Controller
 {
     public function store(Request $request)
     {
-        event(new ChatMessage($request->body));
+//        event(new ChatMessage($request->body));
+
+//        $chat->notify(new \App\Notifications\ChatMessage('Hola que tal!'));
+        Notification::send(Auth::user(),new \App\Notifications\ChatMessage($request->body));
     }
 
 }
